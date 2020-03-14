6f88b0ede83f5d6a4db9e3b0b04154f195e758e0
emi <ejhartline@gmail.com>
2020-03-14 17:25:18 -0400

    aesthetic changes to html, started adding ids to html elements for js DOM grabbing, and biggest of all, got the time and date to update itself in browser (before, only showed current time/date when refreshed)

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+// Setting global variable
+var date;
 
-// Creating time/date variables
-var currentDate = moment().format("dddd, MMMM Do");
-var currentTime = moment().format("LTS");
+// Grabbing elements from the DOM
+var currentDate = $("#current-date");
+var currentTime = $("#current-time");
 
-console.log(currentDate);
-console.log(currentTime);
+// Defining functions
+var updateDateTime = function () {
+    date = moment();
+    currentDate.text(date.format("dddd, MMMM Do"));
+    currentTime.text(date.format("h : mm A"));
+};
 
-// Display current date
-$("#current-date").text(currentDate);
+// Getting the document ready. Functions defined above will be called/run below
+$(document).ready(function(){
+    // Update and render the current date and time
+    updateDateTime();
+    // Setting the update to happen every second so that the time is dynmically changing itself in the browser
+    setInterval(updateDateTime, 1000);
+});
+
+
+
