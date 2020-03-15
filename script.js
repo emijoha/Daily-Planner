805ba178a56986e7205a571568a387fd1ffe4684
emi <ejhartline@gmail.com>
2020-03-15 16:22:02 -0400

    regorganized js file, put all variables and functions in the document ready function, re-defined the updatePlanner functions set to have variable iterates within each function, color-coding now works on its own dynamically, without refreshing page, to change color of textblocks as time goes by

--- script.js
+++ script.js
@@ -1,140 +1,186 @@
-// Setting global variables
+// Getting the document ready.
 // ________________________________________________________________________________________________________________________
-var currentDate = moment();
-// currentHour is a number value of 24hr clock. 
-// HTML text and displays will be in 12hr clock, but JS times will be in 24hr clock to better set up conditionals
-var currentHour = parseInt(currentDate.format("HH"));
 
-
-// Grabbing elements from the DOM
-// ________________________________________________________________________________________________________________________
-var currentDateDisplay = $("#current-date");
-var currentTimeDisplay = $("#current-time");
-// Textareas for each timeblock
-var text9 = $("#event9");
-var text10 = $("#event10");
-var text11 = $("#event11");
-var text12 = $("#event12");
-var text13 = $("#event13");
-var text14 = $("#event14");
-var text15 = $("#event15");
-var text16 = $("#event16");
-var text17 = $("#event17");
-var textareas = [text9, text10, text11, text12, text13, text14, text15, text16, text17];
-// Buttons Images for each timeblock
-var btn9 = $("#btn9");
-var btn10 = $("#btn10");
-var btn11 = $("#btn11");
-var btn12 = $("#btn12");
-var btn13 = $("#btn13");
-var btn14 = $("#btn14");
-var btn15 = $("#btn15");
-var btn16 = $("#btn16");
-var btn17 = $("#btn17");
-var buttonImages = [btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16, btn17];
-// button ELEMENTS of each timeblock
-var btnEl9 = $(".9");
-var btnEl10 = $(".10");
-var btnEl11 = $(".11");
-var btnEl12 = $(".12");
-var btnEl13 = $(".13");
-var btnEl14 = $(".14");
-var btnEl15 = $(".15");
-var btnEl16 = $(".16");
-var btnEl17 = $(".17");
-var btnElements = [btnEl9, btnEl10, btnEl11, btnEl12, btnEl13, btnEl14, btnEl15, btnEl16, btnEl17];
-
-// Defining functions
-// ________________________________________________________________________________________________________________________
-// getting the current date and time from moment.js and setting displays to match
-var updateDateTimeColor = function () {
-    // calling reset, planner will reset when time is 1 am
-    plannerReset();
-    //call moment() to access current time
-    currentDate = moment();
-    currentDateDisplay.text(currentDate.format("dddd, MMMM Do"));
-    currentTimeDisplay.text(currentDate.format("h : mm A"));
-    // Check hour stamp of each timeblock to assign color code
-    // hardcoding "9" instead of array length for ease, as it is also 9 in other for loops
-    for (var i = 0; i < 9; i++) {
-        // textTime is number value of textareas' data-time in 24hr clock
-        var textTime = parseInt(textareas[i][0].dataset.time);
-        if (textTime === currentHour) {
-            textareas[i].addClass("current-time").removeClass("future-time past-time");
-            textareas[i].removeProp("disabled");
-            btnElements[i].removeProp("disabled");
-        }
-        else if (textTime < currentHour) {
-            textareas[i].addClass("past-time").removeClass("future-time current-time");
-            // adding "disabled" to all past/outside office hours timeblocks
-            textareas[i].prop("disabled", true);
-            btnElements[i].prop("disabled", true);
-        }
-        else if (textTime > currentHour) {
-            textareas[i].addClass("future-time").removeClass("current-time past-time");
-            textareas[i].removeProp("disabled");
-            btnElements[i].removeProp("disabled");
-        }
-    }
-};
-
-// Save the cooresponing timeblock Input of each button click
-var saveEvent = function () {
-    event.preventDefault();
-
-    for (var i = 0; i < 9; i++) {
-        // creating variables that grab the button and the textarea's matching ids
-        var buttonId = event.target.dataset.match;
-        var textId = textareas[i][0].id;
-        // will create localStorage key for particular button click AND ONLY its text input
-        if (buttonId === textId) {
-            var textInput = textareas[i][0].value;
-            var eventHour = "event" + (i + 9);
-            localStorage.setItem(eventHour, textInput);
-        }
-    }
-    // render events that were just saved
-    renderEvents();
-};
-
-// This function, which renders events, will be called inside the click event fucntion (saveEvent)
-// it will also be called on its own to rerender saved events if page is refreshed
-var renderEvents = function () {
-    for (var i = 0; i < 9; i++) {
-        var eventHour = "event" + (i + 9);
-        var textDisplay = textareas[i];
-        textDisplay[0].value = localStorage.getItem(eventHour);
-    }
-};
-
-// This function will reset the planner after midnight (at 1 am)
-// It will run continuously inside the updateDateTimeColor function
-var plannerReset = function () {
-    // if the time is 1
-    if (currentHour === 1) {
-        for (var i = 0; i < 9; i++) {
-            textareas[i][0].value = 0;
-            var eventHour = "event" + (i + 9);
-            localStorage.removeItem(eventHour);
-        }
-    }
-};
-
-
-
-// Getting the document ready. Functions defined above will be called/run below
-// ________________________________________________________________________________________________________________________
 $(document).ready(function () {
-    // Update and render the current date and time, and timeblock color code
-    updateDateTimeColor();
-    // render previously saved events for all timeblocks
+
+    // Setting global variables
+    // ________________________________________________________________________________________________________________________
+    var currentDate = moment();
+    // currentHour is a number value of 24hr clock. 
+    // HTML text and displays will be in 12hr clock, but JS times will be in 24hr clock to better set up conditionals
+    var currentHour = parseInt(currentDate.format("HH"));
+
+
+    // Grabbing elements from the DOM
+    // ________________________________________________________________________________________________________________________
+    var currentDateDisplay = $("#current-date");
+    var currentTimeDisplay = $("#current-time");
+    // Textareas for each timeblock
+    var text9 = $("#event9");
+    var text10 = $("#event10");
+    var text11 = $("#event11");
+    var text12 = $("#event12");
+    var text13 = $("#event13");
+    var text14 = $("#event14");
+    var text15 = $("#event15");
+    var text16 = $("#event16");
+    var text17 = $("#event17");
+    var textareas = [text9, text10, text11, text12, text13, text14, text15, text16, text17];
+    // Buttons Images for each timeblock
+    var btn9 = $("#btn9");
+    var btn10 = $("#btn10");
+    var btn11 = $("#btn11");
+    var btn12 = $("#btn12");
+    var btn13 = $("#btn13");
+    var btn14 = $("#btn14");
+    var btn15 = $("#btn15");
+    var btn16 = $("#btn16");
+    var btn17 = $("#btn17");
+    var buttonImages = [btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16, btn17];
+    // button ELEMENTS of each timeblock
+    var btnEl9 = $(".9");
+    var btnEl10 = $(".10");
+    var btnEl11 = $(".11");
+    var btnEl12 = $(".12");
+    var btnEl13 = $(".13");
+    var btnEl14 = $(".14");
+    var btnEl15 = $(".15");
+    var btnEl16 = $(".16");
+    var btnEl17 = $(".17");
+    var btnElements = [btnEl9, btnEl10, btnEl11, btnEl12, btnEl13, btnEl14, btnEl15, btnEl16, btnEl17];
+
+
+    // DEFINING FUNCTIONS
+    // ________________________________________________________________________________________________________________________
+    
+    // getting the current date and time from moment.js and setting displays to match
+    var updateDateTime = function () {
+        //call moment() to access current time
+        currentDate = moment();
+        currentDateDisplay.text(currentDate.format("dddd, MMMM Do"));
+        currentTimeDisplay.text(currentDate.format("h : mm A"));
+    };
+
+
+    // This function determines background color of each timeblock as time goes by
+    var colorCode = function () {
+        // Check hour stamp of each timeblock to assign color code
+        currentDate = moment();
+        currentHour = parseInt(currentDate.format("HH"));
+        // hardcoding "9" instead of array length for ease, as it is also 9 in other for loops
+        for (var i = 0; i < 9; i++) {
+            // textTime is number value of textareas' data-time in 24hr clock
+            var textTime = parseInt(textareas[i][0].dataset.time);
+            if (textTime === currentHour) {
+                textareas[i].addClass("current-time").removeClass("future-time past-time");
+                textareas[i].removeProp("disabled");
+                btnElements[i].removeProp("disabled");
+                buttonImages[i].removeProp("disabled");
+            }
+            else if (textTime < currentHour) {
+                textareas[i].addClass("past-time").removeClass("future-time current-time");
+                // adding "disabled" to all past/outside office hours timeblocks
+                textareas[i].prop("disabled", true);
+                btnElements[i].prop("disabled", true);
+                buttonImages[i].prop("disabled", true);
+            }
+            else {
+                textareas[i].addClass("future-time").removeClass("current-time past-time");
+                textareas[i].removeProp("disabled");
+                btnElements[i].removeProp("disabled");
+                buttonImages[i].removeProp("disabled");
+            }
+        }
+    }
+
+
+    // Save the cooresponing timeblock Input of each button click
+    var saveEvent = function () {
+
+        event.preventDefault();
+
+        for (var i = 0; i < 9; i++) {
+
+            // creating variables that grab the button and the textarea's matching ids
+            var buttonId = event.target.dataset.match;
+            var textId = textareas[i][0].id;
+
+            // will create localStorage key for particular button click AND ONLY its text input
+            if (buttonId === textId) {
+                var textInput = textareas[i][0].value;
+                var eventHour = "event" + (i + 9);
+                localStorage.setItem(eventHour, textInput);
+            }
+
+        }
+
+        // render events that were just saved
+        renderEvents();
+    };
+
+
+    // This function, which renders events, will be called inside the click event fucntion (saveEvent)
+    // it will also be called on its own to rerender saved events if page is refreshed
+    var renderEvents = function () {
+
+        for (var i = 0; i < 9; i++) {
+
+            var eventHour = "event" + (i + 9);
+            var textDisplay = textareas[i];
+
+            textDisplay[0].value = localStorage.getItem(eventHour);
+        }
+
+    };
+
+
+    // This function will reset the planner after midnight (at 1 am)
+    // It will run continuously inside the updateDateTimeColor function
+    var plannerReset = function () {
+
+        currentDate = moment();
+        currentHour = parseInt(currentDate.format("HH"));
+
+        // if the time is 1
+        if (currentHour === 1) {
+            for (var i = 0; i < 9; i++) {
+                textareas[i][0].value = 0;
+                var eventHour = "event" + (i + 9);
+                localStorage.removeItem(eventHour);
+            }
+        }
+
+        else {
+            return;
+        }
+
+    };
+
+
+    // Collecting functions that require interval into one function, this will be set on interval
+    var updatePlanner = function () {
+
+        updateDateTime();
+        plannerReset();
+        colorCode();
+
+    };
+
+
+    // RUNNING THE FUNCTIONS DEFINED ABOVE
+    // ________________________________________________________________________________________________________________________
+    // Update planner
+    plannerReset();
+    updateDateTime();
+    colorCode();
     renderEvents();
-    // timeColorCode();
+
     // Setting the update to happen every second so that the time and color codes are dynmically changing
-    setInterval(updateDateTimeColor, 1000);
-    // setInterval(timeColorCode, 1000);
+    setInterval(updatePlanner, 1000);
+
     // click event set for all buttons (which are images), with saveEvent function 
     $("img").on("click", saveEvent);
+
 });
 
 
